751cbc0b26b9dc4c1584661ed925d712cbbcb142
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-01 22:46:49 +0800

    [🌈 style]：front page style&error handler

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Wrapper from "./styles/errorPage";
+import img from "@/public/images/not-found.svg";
+import Image from "next/image";
+import Link from "next/link";
+export default function Custom404() {
+  return (
+    <Wrapper>
+      <div>
+        <Image src={img} alt="not found" />
+        <h3>Ohh! page not found</h3>
+        <p>we can't seem to find the page you are looking for</p>
+        <Link href="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
-import Link from 'next/link'
- 
+import Wrapper from "@/app/styles/landing";
+import logo from "@/public/images/logo.svg";
+import main from "@/public/images/main.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import Loading from "./loading";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 export default function Page() {
-  return <Link href="/dashboard">Dashboard</Link>
+  return (
+    <Suspense fallback={<Loading />}>
+      <Wrapper>
+        <nav>
+          <Image src={logo} alt="logo"></Image>
+        </nav>
+        <div className="container page">
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+              narwhal.
+            </p>
+            <Link href="/register" className="btn register-link">
+              Register
+            </Link>
+            <Link href="/login" className="btn ">
+              Login / Demo User
+            </Link>
+          </div>
+          <Image
+            src={main}
+            width={400}
+            height={353}
+            alt="job hunt"
+            className="img main-img"
+          />
+        </div>
+      </Wrapper>
+    </Suspense>
+  );
 }
