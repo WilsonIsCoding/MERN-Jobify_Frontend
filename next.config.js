2cac97ad53657c8940431ccd27c1d44af57d4cc7
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-07 16:51:17 +0800

    [✨ feat]：setup proxy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5100/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
