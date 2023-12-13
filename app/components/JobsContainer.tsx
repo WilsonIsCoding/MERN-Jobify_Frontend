fcea452d3e647a0b5350ad1c6810e004913933d7
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-13 14:33:53 +0800

    [✨ feat]：job container component

--- app/components/JobsContainer.tsx
+++ app/components/JobsContainer.tsx
@@ -1,9 +1,20 @@
 import Wrapper from "../styles/JobsContainer";
+import Job from "./Job";
+import { useAllJobsContext } from "../(pages)/dashboard/all-jobs/page";
 const JobsContainer = () => {
-  return (
-    <>
-      <h1>jobsContainer</h1>
-    </>
-  );
+  const { jobs } = useAllJobsContext();
+  if (jobs.length == 0) {
+    return <Wrapper>No Jobs To Display...</Wrapper>;
+  } else {
+    return (
+      <Wrapper>
+        <div className="jobs">
+          {jobs.map((job:object) => {
+            return <Job key={job._id} {...job} />;
+          })}
+        </div>
+      </Wrapper>
+    );
+  }
 };
 export default JobsContainer;
