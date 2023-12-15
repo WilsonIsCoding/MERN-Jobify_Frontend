304d6332d47083510dfeb2636a978aab57ddff17
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-15 20:32:58 +0800

    [✨ feat]：admin page

--- app/(pages)/dashboard/admin/page.tsx
+++ app/(pages)/dashboard/admin/page.tsx
@@ -1,4 +1,47 @@
 "use client";
-export default function Page() {
-  return <>Admin Page</>;
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import customFetch from "@/app/utils/fetchUtils";
+import Wrapper from "@/app/styles/StatsContainer";
+import { toast } from "react-toastify";
+import StatItem from "@/app/components/StatItem";
+import { useEffect, useState } from "react";
+export const fetchUserData = async () => {
+  try {
+    const response = await customFetch.get("/users/admin/app-stats");
+    return response.data;
+  } catch (error) {
+    toast.error("You are not authorized to view this page");
+  }
+};
+export default function Admin() {
+  const [adminData, setAdminData] = useState({ users: 0, job: 0 });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchUserData();
+        setAdminData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Wrapper>
+      <StatItem
+        title="current users"
+        count={adminData?.users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={adminData?.job}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
+    </Wrapper>
+  );
 }
