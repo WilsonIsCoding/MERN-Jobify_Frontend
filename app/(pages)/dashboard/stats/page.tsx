1b1bf07e62a9880ca396dbbccb83ea678fc1eff2
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-17 12:21:37 +0800

    [✨ feat]：chartsContainer

---
+++ app/(pages)/dashboard/stats/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
+import { ChartsContainer, StatsContainer } from "@/app/components";
+import customFetch from "@/app/utils/fetchUtils";
+import { toast } from "react-toastify";
+export const fetchStatsData = async () => {
+  try {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  } catch (error) {
+    toast.error("You are not authorized to view this page");
+  }
+};
+export default function Page() {
+  const [statsData, setStatsData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { defaultStats, monthlyApplications } = await fetchStatsData();
+        setStatsData({ defaultStats, monthlyApplications });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      <StatsContainer defaultStats={statsData.defaultStats} />
+      {statsData.monthlyApplications?.length > 1 && (
+        <ChartsContainer data={statsData.monthlyApplications} />
+      )}
+    </>
+  );
+}
